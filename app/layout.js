b3648fab384ffbb15dd35bd835a178450fd9979f
mineshopdev <mineshop47@gmail.com>
2024-02-07 16:27:26 +0100

    Added Vercel Speed Insights

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import GoogleAnalytics from "./Components/GoogleAnalytics";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,6 +18,7 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
